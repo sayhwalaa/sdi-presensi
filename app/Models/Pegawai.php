21bc8938a5c25aa29dae733d44959a4ab44e9d09
Muhamad Ardalepa <69201192+muhamadArdalepa@users.noreply.github.com>
2023-01-11 07:38:00 +0700

    merge with task

--- app/Models/Pegawai.php
+++ app/Models/Pegawai.php
@@ -20,15 +20,14 @@ class Pegawai extends Model
     {
         return $this->belongsTo(Cabang::class);
     }
-<<<<<<< HEAD
 
     public function jabatan()
     {
         return $this->belongsTo(Cabang::class);
-=======
-    public function task(){
+    }
+    public function task()
+    {
 
         return $this->belongsToMany(Task::class)->withTimestamps();
->>>>>>> 611f5f488d963fb5dcc08d01d39fc3327fd42924
     }
 }
